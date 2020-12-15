a7941d19a26f379ef0844eb636ea17753c13e870
Gabriel Guldner <gabriel@guldner.eu>
2020-12-15 19:52:34 +0100

    Add spring boot

---
+++ subsystem1/server/src/main/java/at/softeng/four/SubsystemOneServer.java
@@ -0,0 +1,11 @@
+package at.softeng.four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsystemOneServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SubsystemOneServer.class, args);
+    }
+}
